f481c35f7109ceed1cf4131fcb006e1ac3bd3f41
David Bootle <davidtbootle@gmail.com>
2021-11-25 17:08:16 -0500

    App now loads selenium managers from site config files

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import asyncio
 import json
 import sys
 from emailmanager import EmailManager
 from seleniummanager import SeleniumManager
+from pathlib import Path
 
 # Load json config
 
 class App:
 
     def __init__(self):
+        self.selenium_managers: dict[str, SeleniumManager] = {}
         self.load_config_file()
 
-        self.email_manager = EmailManager(self.config['smtpServer'], self.config['sendFrom'])
+        self.email_manager: EmailManager = EmailManager(self.config['smtpServer'], self.config['sendFrom'])
+        self.load_sites()
 
     def load_config_file(self):
         '''Loads and validates config information from config.json.'''
@@ -18,26 +22,102 @@ class App:
             with open('config.json') as config_file:
                 try:
                     self.config = json.load(config_file)
-                    if not self.config.get('smtpServer'):
+                    if not self.config.get('smtpServer'): # if the smtpServer doesn't exist, exit with an error message
                         print('ERROR: config.json must contain smtpServer options. See documentation for details.')
                         sys.exit()
-                    if not self.config.get('sendFrom'):
+                    if not self.config.get('sendFrom'): # if the sendFrom object doesn't exist, exit with an error message
                         print('ERROR: config.json must contain sendFrom options. See documentation for details.')
                         sys.exit()
-                except json.decoder.JSONDecodeError as error:
+                except json.decoder.JSONDecodeError as error: # if there is an error while decoding the json, exit with an error message
                     print("ERROR: config.json is not a valid json file. See message below for decoder response.")
                     print("Message:", error)
                     sys.exit()
-        except FileNotFoundError:
+        except FileNotFoundError: # if config.json doesn't exit, exit with an error message
             print("ERROR: Must have a config.json file in the application directory.")
             sys.exit()
-        
+    
+    def load_sites(self):
+        '''Loads selenium managers from the sites config files.'''
         # start loading sites
+        sites_dir = Path('.') / 'sites'
+        if not sites_dir.exists(): # if the sites directory doesn't exist, exit with an error message
+            print("ERROR: 'sites' directory is required. See documentation for more details.")
+            sys.exit()
+        sites = sites_dir.glob('*.json')
+        for site in sites:
+            site_name = site.name[:-5] # removes the .json from the file name
+            with site.open() as site_contents:
+                try:
+                    contents = json.load(site_contents)
+                except json.decoder.JSONDecodeError as error:
+                    print(f"ERROR: site configuration '{site.name}' is not valid JSON. See message below for decoder response.")
+                    print("Message:", error)
+                    sys.exit()
+            self.validate_site_config(contents, site.name)
+            send_to = []
+            for block in contents['sendTo']:
+                email_address = block['emailAddress']
+                display_name = block.get('displayName')
+                display_name = display_name if display_name else email_address
+                send_to.append({
+                    'email_address': email_address,
+                    'display_name': display_name
+                })
+                self.selenium_managers[site_name] = SeleniumManager(
+                    contents['name'],
+                    contents['url'], 
+                    contents['elementXPath'],
+                    contents['testType'],
+                    contents['compareValue'],
+                    send_to,
+                    contents['reloadTime'],
+                    contents['waitTime'],
+                    self.email_manager)
+    
+    def validate_site_config(self, config: dict[str, any], file_name):
+        if not config.get("name"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'name'. See documentation for more details.")
+            sys.exit()
+        if not config.get("url"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'url'. See documentation for more details.")
+            sys.exit()
+        if not config.get("elementXPath"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'elementXPath'. See documentation for more details.")
+            sys.exit()
+        if not config.get("testType"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'testType'. See documentation for more details.")
+            sys.exit()
+        if not config.get("compareValue"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'compareValue'. See documentation for more details.")
+            sys.exit()
+        if not config.get("sendTo"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'sendTo'. See documentation for more details.")
+            sys.exit()
+        if type(config['sendTo']) != list:
+            print(f"ERROR: site configuarion '{file_name}' is invalid. Parameter 'sendTo' must be a list. See documentation for more details.")
+            sys.exit()
+        if len(config['sendTo']) == 0:
+            print(f"WARNING: site configuration '{file_name} has no sendTo values. No notification will be sent!")
+        for index, obj in enumerate(config['sendTo']):
+            if not obj.get("emailAddress"):
+                print(f"ERROR: site configuarion '{file_name}' is invalid. 'sendTo' block {index + 1} is missing required parameter 'emailAddress'. See documentation for more details.")
+                sys.exit()
+        if not config.get("reloadTime"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'reloadTime'. See documentation for more details.")
+            sys.exit()
+        if not config.get("waitTime"):
+            print(f"ERROR: site configuarion '{file_name}' is missing required parameter 'waitTime'. See documentation for more details.")
+            sys.exit()
 
     def run(self):
         '''Starts running the app. This includes starting up webdrivers and actively performing testing.'''
-        pass
+        
+        # start selenium managers
+        for key in self.selenium_managers:
+            self.selenium_managers[key].start()
+            asyncio.run(self.selenium_managers[key].run())
 
 if __name__ == "__main__":
+    print('Starting app!')
     app = App()
-    print('doing things!')
+    app.run()
--- seleniummanager.py
+++ seleniummanager.py
@@ -1,29 +1,45 @@
+import asyncio
 from selenium import webdriver
-import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import selenium.common.exceptions as selenium_exceptions
 
+from emailmanager import EmailManager
+
 class SeleniumManager:
     '''A class that is in charge of a single site. It manages its own webdriver and testing.'''
 
-    def __init__(self, name: str, url: str, element_xpath: str, test_type: str, compare_value: str, send_to: list[object]):
+    driver: webdriver.Firefox
+
+    def __init__(self, name: str, url: str, element_xpath: str, test_type: str, compare_value: str, send_to: list[object], reload_time: int | float, wait_time: int | float, email_manager: EmailManager):
         '''Initialize a new SeleniumManager'''
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(10) #will wait up to 10 seconds for the element to be found before an error occurs
         self.name = name
         self.url = url
         self.element_xpath = element_xpath
         self.test_type = test_type
         self.compare_value = compare_value
         self.send_to = send_to
+        self.reload_time = reload_time
+        self.wait_time = wait_time
+        self.set_test()
+        self.disabled = False
     
-    def run(self):
+    def start(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(self.wait_time) #will wait up to 10 seconds for the element to be found before an error occurs
+
+    async def run(self):
+        '''Starts the run loop.'''
+        while not self.disabled:
+            self.test()
+            await asyncio.sleep(self.reload_time)
+    
+    def set_test(self):
         '''Run one test.'''
         match self.test_type:
             case 'element_does_not_contain_text':
                 # checks the element to see if the element's text contains the compare value. If not, it triggers the success response.
-                self.element_does_not_contain_text_test()
+                self.test = self.element_does_not_contain_text_test
             case _:
                 self.failure_response(f'[ERROR] Site {self.name} is configured with a test type that is not valid. Please check the documentation for a list of valid test types.')
                 self.stop()
@@ -47,10 +63,10 @@ class SeleniumManager:
         message_end = ''
 
         match type(exception):
-            case type(selenium_exceptions.NoSuchElementException):
+            case selenium_exceptions.NoSuchElementException:
                 message_end = "The element could not be found on the page. This could be because the time the page takes to load exceeds the page wait time, or that your XPath is invalid. Check your configuration."
             case _ as error:
-                message_end = f"A {type(error)} occurred. Error output message: {str(error)}"
+                message_end = f"A {type(exception)} occurred. Error output message: {str(exception)}"
         
         self.failure_response(message_start + message_end)
     
@@ -63,10 +79,11 @@ class SeleniumManager:
         print(f'Site {self.name} triggered success response!')
         self.stop()
     
-    def normal_response(self, message):
+    def normal_response(self):
         '''This function triggers when the test criteria is not met, but no errors occur, and the program can continue running.'''
         print(f'Site {self.name} tested negative and will continue running.')
 
     def stop(self):
         '''Stops the webdriver. Should also perform any necessary cleanup.'''
         self.driver.quit()
+        self.disabled = True
